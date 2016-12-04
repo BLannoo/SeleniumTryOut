4e5ba5fe0a389c1bc544bea86df4773c60479417
BLannoo <brunolannoo@gmail.com>
2016-12-04 14:52:10 +0100

    brunol fix the original test

--- SeleniumTryOut/src/test/java/selenium/MtgStocksTest.java
+++ SeleniumTryOut/src/test/java/selenium/MtgStocksTest.java
@@ -2,19 +2,37 @@ package selenium;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import junit.framework.TestCase;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
 
-public class MtgStocksTest extends TestCase {
+public class MtgStocksTest {
 
 	private static final String URL_MTGSTOCKS = "http://www.mtgstocks.com/";
 	private static final String PAGE_TITLE = "MTGStocks.com";
+	private WebDriver driver;
 
+	@BeforeClass
+	public static void setUpClass() {
+		FirefoxDriverManager.getInstance().setup();
+	}
+	
+	@Before
+	public void setUp() {
+		driver = new FirefoxDriver();
+	}
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test
 	public void testApp() {
-		WebDriver driver = new FirefoxDriver();
 		driver.get(URL_MTGSTOCKS);
 		assertThat(driver.getTitle()).isEqualTo(PAGE_TITLE);
 	}
