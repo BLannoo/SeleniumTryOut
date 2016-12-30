7ca4dd5dbcdc021024d7853e0192f51413046cbc
BLannoo <brunolannoo@gmail.com>
2016-12-30 16:02:27 +0100

    restarted from simplest test

--- SeleniumTryOut/src/test/java/selenium/mtgstocks/MtgStocksTest.java
+++ SeleniumTryOut/src/test/java/selenium/mtgstocks/MtgStocksTest.java
@@ -1,17 +1,37 @@
 package selenium.mtgstocks;
 
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import selenium.SeleniumWebDriverTest;
-import selenium.mtgstocks.pages.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class MtgStocksTest extends SeleniumWebDriverTest {
+public class MtgStocksTest {
+
+	protected WebDriver driver;
+
+	@BeforeClass
+	public static void setUpClass() {
+		FirefoxDriverManager.getInstance().setup();
+	}
+
+	@Before
+	public void setUp() {
+		driver = new FirefoxDriver();
+	}
 
 	@Test
 	public void testApp() {
-		new HomePage(driver)
-			.assertOnPage()
-			.searchForCard(MtgCards.BLACK_LOTUS)
-			.assertOnPage();
+		driver.get("http://www.mtgstocks.com");
+		Assertions.assertThat(driver.getTitle()).isEqualTo("MTGStocks.com");
+	}
+
+	@After
+	public void tearDown() {
+		driver.quit();
 	}
 }
