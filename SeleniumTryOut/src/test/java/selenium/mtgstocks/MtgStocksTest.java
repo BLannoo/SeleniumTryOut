e135677a66064a43e13fababe2686605efc2d140
BLannoo <brunolannoo@gmail.com>
2016-12-30 16:31:35 +0100

    test mtgstocks home page

--- SeleniumTryOut/src/test/java/selenium/mtgstocks/MtgStocksTest.java
+++ SeleniumTryOut/src/test/java/selenium/mtgstocks/MtgStocksTest.java
@@ -1,13 +1,16 @@
 package selenium.mtgstocks;
 
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import org.assertj.core.api.AbstractCharSequenceAssert;
 import org.assertj.core.api.Assertions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MtgStocksTest {
@@ -25,9 +28,25 @@ public class MtgStocksTest {
 	}
 
 	@Test
-	public void testApp() {
+	public void homePage() {
 		driver.get("http://www.mtgstocks.com");
+		assertTitle();
+		assertGreetingText();
+		assertNavigationBar();
+	}
+
+	private void assertTitle() {
 		Assertions.assertThat(driver.getTitle()).isEqualTo("MTGStocks.com");
+	}
+
+	private void assertGreetingText() {
+		Assertions.assertThat(driver.findElement(By.xpath("//div[@class='jumbotron']/p")).getText())
+				.isEqualTo("Your favorite resource for daily price updates on Magic: the Gathering cards. We track prices of all cards and report the ones that have recently fluctuated the most for your Magic finance convenience!");
+	}
+
+	private void assertNavigationBar() {
+		Assertions.assertThat(driver.findElement(By.id("navbar")).getText())
+				.isEqualTo("Sets\nInterests\nAnalytics\nDecks\nAbout\nLogin\nSign up");
 	}
 
 	@After
